4536dc4ac1b8436bc302ec2a6b58127684af97aa
Blunderstab <phoenix_of_faith@hotmail.com>
2018-12-01 13:37:55 -0600

    towns

---
+++ Assets/Scripts/Towns/TownData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownData : MonoBehaviour
+{
+
+    public string townName;
+    public int population;
+    public float military;
+    public float security;
+    public float zealotry;
+    public float disciples;
+    public float notoriety;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
